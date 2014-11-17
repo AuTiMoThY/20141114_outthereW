c45612f52923a868aaae88f92cf472e03443e01c
AuTiMoThY <m62466@gmail.com>
2014-11-17 22:18:13 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="zh"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="zh"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="zh"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="zh"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="zh"><!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>關於我們 ｜ 好野</title>
+
+<?php
+// -------------------------------
+// SEO
+// CSS
+// Script in the HEAD
+// -------------------------------
+  include_once 'inc/HEAD.php';
+ ?>
+
+
+<?php
+//app
+  include_once 'inc/Social.php';
+ ?>
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+
+<body class="about_page">
+
+<div id="ip-container" class="ip-container">
+
+    <!-- main content -->
+    <div class="ip-main">
+		<div id="pageBanner" class="page_banner"><h1 class="hide_txt">關於我們 about</h1></div>
+
+
+<?php
+// -------------------------------
+// Header module
+// -------------------------------
+  include_once 'inc/Header.php';
+ ?>
+
+		<article id="main">
+			<section class=" wrapper">
+				<img src="images/about_content.jpg" alt="關於好野">
+			</section>
+		</article>
+
+<?php
+// -------------------------------
+// Footer module
+// -------------------------------
+  include_once 'inc/Footer.php';
+ ?>
+
+    </div><!-- /.ip-main  END  !! -->
+</div><!-- /container -->
+
+
+<?php
+// -------------------------------
+// Script in the FOOT
+// -------------------------------
+  include_once 'inc/ScriptFOOT.php';
+ ?>
+
+<?php
+// -------------------------------
+// google analytics
+// -------------------------------
+  include_once 'inc/GA.php';
+ ?>
+
+</body>
+</html>
---
+++ productList.php
@@ -0,0 +1,349 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="zh"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="zh"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="zh"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="zh"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="zh"><!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>商品列表 ｜ 好野</title>
+
+<?php
+// -------------------------------
+// SEO
+// CSS
+// Script in the HEAD
+// -------------------------------
+  include_once 'inc/HEAD.php';
+ ?>
+
+
+<?php
+//app
+  include_once 'inc/Social.php';
+ ?>
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+
+<body class="productList_page cnt_page">
+
+<div id="ip-container" class="ip-container">
+
+    <!-- main content -->
+    <div class="ip-main">
+		<!-- <div id="pageBanner" class="page_banner"><h1 class="hide_txt">商品列表 about</h1></div> -->
+
+
+<?php
+// -------------------------------
+// Header module
+// -------------------------------
+  include_once 'inc/Header.php';
+ ?>
+
+		<article id="main">
+			<section class=" wrapper">
+				<aside id="LeftAside">
+					<header>商品分類</header>
+					<nav class="prod_class_nav prod_class">
+						<ul>
+							<li class="active"><a href="productList.php" class="prod_class1 btn">帳篷</a></label></li>
+							<li class=""><a href="productList.php" class="prod_class2 btn">睡袋</a></label></li>
+							<li class=""><a href="productList.php" class="prod_class3 btn">配件</a></label></li>
+						</ul>
+					</nav>
+				</aside>
+
+				<div class="prodList-wrap">
+					<span class="path">
+						<a href="index.php">首頁</a>&gt;
+						<a href="product.php">商品介紹</a>&gt;
+						<span class="txt4">帳篷</span>
+					</span>
+					<section class="prodList-container">
+						<ul class="cf">
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><div style="background-image: url('images/temp/prod4.jpg');"></div></div>
+									<figcaption>
+										<h2 class="prod_name">
+											實惠型6人雙門蒙古包帳篷 露營用品.4人帳篷.6人帳篷.蒙古包
+										</h2>
+										<div class="price">
+											$ <span class="txt3">3980</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod2.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本DOPPELGANGER營舞者】紫羅蘭充氣帳
+										</h2>
+										<div class="price">
+											$ <span class="txt3">12500</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod3.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本品牌LOGOS】PANEL抗風進化系-綠楓XL 270五人帳篷蓬...
+										</h2>
+										<div class="price">
+											$ <span class="txt3">4680</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod1.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											實惠型6人雙門蒙古包帳篷 露營用品.4人帳篷.6人帳篷.蒙古包
+										</h2>
+										<div class="price">
+											$ <span class="txt3">3980</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod2.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本DOPPELGANGER營舞者】紫羅蘭充氣帳
+										</h2>
+										<div class="price">
+											$ <span class="txt3">12500</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod3.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本品牌LOGOS】PANEL抗風進化系-綠楓XL 270五人帳篷蓬...
+										</h2>
+										<div class="price">
+											$ <span class="txt3">4680</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod1.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											實惠型6人雙門蒙古包帳篷 露營用品.4人帳篷.6人帳篷.蒙古包
+										</h2>
+										<div class="price">
+											$ <span class="txt3">3980</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod2.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本DOPPELGANGER營舞者】紫羅蘭充氣帳
+										</h2>
+										<div class="price">
+											$ <span class="txt3">12500</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod3.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本品牌LOGOS】PANEL抗風進化系-綠楓XL 270五人帳篷蓬...
+										</h2>
+										<div class="price">
+											$ <span class="txt3">4680</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod1.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											實惠型6人雙門蒙古包帳篷 露營用品.4人帳篷.6人帳篷.蒙古包
+										</h2>
+										<div class="price">
+											$ <span class="txt3">3980</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod2.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本DOPPELGANGER營舞者】紫羅蘭充氣帳
+										</h2>
+										<div class="price">
+											$ <span class="txt3">12500</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod3.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本品牌LOGOS】PANEL抗風進化系-綠楓XL 270五人帳篷蓬...
+										</h2>
+										<div class="price">
+											$ <span class="txt3">4680</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><img src="images/temp/prod1.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											實惠型6人雙門蒙古包帳篷 露營用品.4人帳篷.6人帳篷.蒙古包
+										</h2>
+										<div class="price">
+											$ <span class="txt3">3980</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><div style="background-image: url('images/temp/prod4.jpg');"></div></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本DOPPELGANGER營舞者】紫羅蘭充氣帳
+										</h2>
+										<div class="price">
+											$ <span class="txt3">12500</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+							<li>
+								<figure><a href="productCnt.php" class="prod_item btn">
+									<div class="pic"><div style="background-image: url('images/temp/prod4.jpg');"></div></div>
+									<div class="pic"><img src="images/temp/prod4.jpg" alt=""></div>
+									<figcaption>
+										<h2 class="prod_name">
+											【日本品牌LOGOS】PANEL抗風進化系-綠楓XL 270五人帳篷蓬...
+										</h2>
+										<div class="price">
+											$ <span class="txt3">4680</span>
+										</div>
+									</figcaption>
+								</a></figure>
+							</li>
+							
+						</ul>
+						<div class="pages_btn">
+							<ul>
+								<li class="prev"><a href=""></a></li>
+								<li class="number"><a href="">1</a></li>
+								<li class="number"><a href="">2</a></li>
+								<li class="number"><a href="">3</a></li>
+								<li class="number"><a href="">4</a></li>
+								<li class="number"><a href="">5</a></li>
+								<li class="next"><a href=""></a></li>
+							</ul>
+						</div>
+					</section>
+				</div>
+			</section>
+		</article>
+
+<?php
+// -------------------------------
+// Footer module
+// -------------------------------
+  include_once 'inc/Footer.php';
+ ?>
+
+    </div><!-- /.ip-main  END  !! -->
+</div><!-- /container -->
+
+
+<?php
+// -------------------------------
+// Script in the FOOT
+// -------------------------------
+  include_once 'inc/ScriptFOOT.php';
+ ?>
+<script>
+$(function () {
+	$("#LeftAside a").hover(function() {
+		// $('#LeftAside li').removeClass();
+		$(this).parent().addClass('class_name');
+	});
+
+
+/*
+	if (location.hash.indexOf("#ProdClass3") != -1) {
+		console.log(location.hash.substr(0));
+		$("#ProdClass3").prop('checked', 'true');
+	}
+	if (location.hash.indexOf("#ProdClass2") != -1) {
+		console.log(location.hash.substr(0));
+		$("#ProdClass2").prop('checked', 'true');
+	}
+	if (location.hash.trim() == "") {
+		console.log(location.hash.substr(0));
+		$("#ProdClass1").prop('checked', 'true');
+	};
+
+	$("input[type=\"radio\"]:checked").parent().addClass('active')*/
+
+})
+</script>
+<?php
+// -------------------------------
+// google analytics
+// -------------------------------
+  include_once 'inc/GA.php';
+ ?>
+
+</body>
+</html>
